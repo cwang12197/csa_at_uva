7bb3a24a4dedcabced26a4ede8b75e821a5cb4bb
cwang12197 <wchristina4@gmail.com>
2023-08-02 15:45:29 -0700

    add images for home

--- csa_at_uva_ui/src/Home/Home.js
+++ csa_at_uva_ui/src/Home/Home.js
@@ -1,12 +1,20 @@
 import './Home.css';
 
 function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <div className="Home">
+            <main>
+                <img src="main background.png" className="parallax bg-img" />
+                <div className="parallax text">
+                    <h2>Welcome to</h2>
+                    <h1>CSA at UVA</h1>
+                    </div>
+                    <img src="mountains_back.png" className="parallax mtn" />
+                    <img src="house_left.png" className="parallax house" />
+
+            </main>
+        </div>
+    );
 }
 
 export default Home;
