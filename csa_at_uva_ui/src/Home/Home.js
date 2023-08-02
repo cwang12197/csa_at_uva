5f3762fee50fbe03f17b857a2719dc530ffa68cf
cwang12197 <wchristina4@gmail.com>
2023-08-02 16:08:05 -0700

    add movement to all layers

--- csa_at_uva_ui/src/Home/Home.js
+++ csa_at_uva_ui/src/Home/Home.js
@@ -5,7 +5,16 @@ function Home() {
     const parallax_el = document.querySelectorAll(".parallax");
     let xVal = 0, yVal = 0;
 
+    window.addEventListener("mousemove", (e) => {
+        xVal = e.clientX - window.innerWidth/2; //detect how far mouse is moving from center of screen instead of top left corner
+        yVal = e.clientY - window.innerHeight/2; 
 
+        //change translate value of layers 
+        parallax_el.forEach((el) => {
+            el.style.transform = `translateX(calc(-50% + ${-xVal}px)) translateY(calc(-50% + ${-yVal}px))`;
+        }) //negative moves in opp direction of mouse
+
+    })
     return (
         <div className="Home">
             <main>
