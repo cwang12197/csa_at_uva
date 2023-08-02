719829dbe5d615ee7652b766ed678c1d10ba1b10
cwang12197 <wchristina4@gmail.com>
2023-08-02 16:37:15 -0700

    change speed on z axis

--- csa_at_uva_ui/src/Home/Home.js
+++ csa_at_uva_ui/src/Home/Home.js
@@ -14,7 +14,11 @@ function Home() {
             let speedX = el.getAttribute('data-speedx');
             let speedY = el.getAttribute('data-speedy');
 
-            el.style.transform = `translateX(calc(-50% + ${-xVal * speedX}px)) translateY(calc(-50% + ${-yVal*speedY}px))`;
+            let zValue = e.clientX - parseFloat(getComputedStyle(el).left);
+
+            el.style.transform = `translateX(calc(-50% + ${-xVal * speedX}px))
+            translateY(calc(-50% + ${-yVal*speedY}px)) 
+            perspective(2300px) translateZ(${zValue}px)`;
         }) //negative moves in opp direction of mouse
 
 
@@ -23,13 +27,13 @@ function Home() {
         <div className="Home">
             <main>
                 <div className="vignette" />
-                <img src="main background.png" data-speedx= "0.23" data-speedY= "0.21" className="parallax bg-img" />
-                <div className="parallax text" data-speedx= "0.15" data-speedY= "0.16">
+                <img src="main background.png" data-speedx= "0.23" data-speedy= "0.25" className="parallax bg-img" />
+                <div className="parallax text" data-speedx= "0.15" data-speedy= "0.16">
                     <h2>Welcome to</h2>
                     <h1>CSA at UVA</h1>
                 </div>
-                <img src="mountains_back.png" data-speedx= "0.12" data-speedY= "0.13" className="parallax mtn" />
-                <img src="house_left.png" data-speedx= "0.17" data-speedY= "0.1" className="parallax house" />
+                <img src="mountains_back.png" data-speedx= "0.12" data-speedy= "0.13" className="parallax mtn" />
+                <img src="house_left.png" data-speedx= "0.17" data-speedy= "0.1" className="parallax house" />
 
             </main>
         </div>
