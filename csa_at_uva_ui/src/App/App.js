251a5f3f6921055ccab5b0adf0f893127aefea67
cwang12197 <wchristina4@gmail.com>
2023-08-02 14:20:59 -0700

    add navbar logo and links

--- csa_at_uva_ui/src/App/App.js
+++ csa_at_uva_ui/src/App/App.js
@@ -1,11 +1,11 @@
-import logo from '../logo.svg';
+import Navbar from '../Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Navbar />
       </header>
     </div>
   );
---
+++ csa_at_uva_ui/src/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <header className="navbar-header">
+        <nav>
+            <img src = "Official-CSA-Square-Logo.png" alt = "Official CSA Logo" class = "logo"/>
+            <ul>
+                <li>
+                    <a href = "#">Home</a>
+                    </li>
+                    <li> <a href = "#">Events</a></li>
+                    <li> <a href = "#">Officers</a></li>
+                   
+                    <li> <a href = "#">Families</a></li>
+                   
+                    <li> <a href = "#">Alumni</a></li>
+                   
+                    <li><a href = "#">Gallery</a></li>
+                    
+            </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Navbar;
