d6d97e0a3571c1d6f1cb83247fc59f1c3e96cd1d
cwang12197 <wchristina4@gmail.com>
2023-08-02 16:04:44 -0700

    init x and y mouse values

--- csa_at_uva_ui/src/Home/Home.js
+++ csa_at_uva_ui/src/Home/Home.js
@@ -1,16 +1,22 @@
 import './Home.css';
 
 function Home() {
+
+    const parallax_el = document.querySelectorAll(".parallax");
+    let xVal = 0, yVal = 0;
+
+
     return (
         <div className="Home">
             <main>
+                <div className="vignette" />
                 <img src="main background.png" className="parallax bg-img" />
                 <div className="parallax text">
                     <h2>Welcome to</h2>
                     <h1>CSA at UVA</h1>
-                    </div>
-                    <img src="mountains_back.png" className="parallax mtn" />
-                    <img src="house_left.png" className="parallax house" />
+                </div>
+                <img src="mountains_back.png" className="parallax mtn" />
+                <img src="house_left.png" className="parallax house" />
 
             </main>
         </div>
