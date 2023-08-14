40cf6cf31b1a8e4c49a5549504630d3d126f6119
cwang12197 <wchristina4@gmail.com>
2023-08-14 18:15:07 -0400

    [officers] rearrange role and name

--- csa_at_uva_ui/src/Officers/Officers.js
+++ csa_at_uva_ui/src/Officers/Officers.js
@@ -9,29 +9,29 @@ function Officers() {
             <div className="exec-officer">
                     <img src="https://storage.googleapis.com/csa-at-uva-files/officers/safak.png" alt = "president" />
                     <div className="exec-info">
-                            <p className="role">President</p>
-                            <p className="name">Safa Khan</p>
+                        <p className="name">Safa Khan</p>
+                        <p className="role">President</p>
                         </div>
                     </div>
                         <div className="exec-officer">
                     <img src="https://storage.googleapis.com/csa-at-uva-files/officers/emmamei.PNG" alt = "vice president"/>
                     <div className="exec-info">
-                            <p className="role">Vice Present</p>
-                            <p className="name">Safa Khan</p>
+                        <p className="name">Safa Khan</p>
+                        <p className="role">Vice Present</p>
                         </div>
                     </div>
                         <div className="exec-officer">
                     <img src="https://storage.googleapis.com/csa-at-uva-files/officers/justinz.png" alt = "treasurer"/>
                     <div className="exec-info">
-                            <p className="role">Treasurer</p>
-                            <p className="name">Safa Khan</p>
+                        <p className="name">Safa Khan</p>
+                        <p className="role">Treasurer</p>
                         </div>
                     </div>
                         <div className="exec-officer">
                     <img src="https://storage.googleapis.com/csa-at-uva-files/officers/oliviaw.png" alt = "secretary"/>
                     <div className="exec-info">
-                            <p className="role">Secretary</p>
-                            <p className="name">Safa Khan</p>
+                        <p className="name">Safa Khan</p>
+                        <p className="role">Secretary</p>
                         </div>
                     </div>
                  </div>
