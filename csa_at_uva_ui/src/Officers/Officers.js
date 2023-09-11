b12474fb174a8bafe7913f90978a656e9b867349
cwang12197 <wchristina4@gmail.com>
2023-09-11 14:47:09 -0400

    [officers] fix officer crop due to improper sizing of width and height container

--- csa_at_uva_ui/src/Officers/Officers.js
+++ csa_at_uva_ui/src/Officers/Officers.js
@@ -3,38 +3,42 @@ import "./Officers.css";
 function Officers() {
     return (
         <div className="officer-container">
-        <h1 className="title">Our Officers</h1>
+            <h1 className="title">Our Officers</h1>
             <p className="title">Here are the officers that organize all the CSA events!</p>
-        <div className="exec-container">
-            <div className="exec-officer">
-                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/safak.png" alt = "president" />
+            <div className="exec-container">
+                <div className="exec-officer">
+                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/safak.png" alt="president" />
                     <div className="exec-info">
                         <p className="name">Safa Khan</p>
                         <p className="role">President</p>
-                        </div>
                     </div>
-                        <div className="exec-officer">
-                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/emmamei.PNG" alt = "vice president"/>
+                </div>
+                <div className="exec-officer">
+                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/emmamei.PNG" alt="vice president" />
                     <div className="exec-info">
                         <p className="name">Safa Khan</p>
                         <p className="role">Vice Present</p>
-                        </div>
                     </div>
-                        <div className="exec-officer">
-                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/justinz.png" alt = "treasurer"/>
+                </div>
+                <div className="exec-officer">
+                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/justinz.png" alt="treasurer" />
                     <div className="exec-info">
                         <p className="name">Safa Khan</p>
                         <p className="role">Treasurer</p>
-                        </div>
                     </div>
-                        <div className="exec-officer">
-                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/oliviaw.png" alt = "secretary"/>
+                </div>
+                <div className="exec-officer">
+                    <img src="https://storage.googleapis.com/csa-at-uva-files/officers/oliviaw.png" alt="secretary" />
                     <div className="exec-info">
                         <p className="name">Safa Khan</p>
                         <p className="role">Secretary</p>
-                        </div>
                     </div>
-                 </div>
+                </div>
+            </div>
+            <div className="community-container">
+                <img className="officer-img" src="https://storage.googleapis.com/csa-at-uva-files/officers/CommPic.jpg" alt="community" />
+            </div>
+
         </div>
     );
 }
